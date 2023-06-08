26091a63c95d83c301611f7fc600b961963fdffb
nursengedik <gediknursen125@gmail.com>
2023-06-08 18:41:12 +0300

    08.06.2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,6 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"html:target/cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         tags = "@regression and @smoke",
